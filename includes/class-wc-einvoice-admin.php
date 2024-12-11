c4175d03ee621941b222d6721f27782185d586de
techsupport-mecaca <77608274+techsupport-mecaca@users.noreply.github.com>
2024-12-11 22:21:12 +0800

    Update class-wc-einvoice-admin.php

--- includes/class-wc-einvoice-admin.php
+++ includes/class-wc-einvoice-admin.php
@@ -186,3 +186,84 @@ class WC_EInvoice_Admin {
         }
     }
 }
+public function __construct() {
+    add_action('admin_init', array($this, 'register_settings'));
+    // ... other existing constructor code
+}
+
+public function register_settings() {
+    register_setting(
+        'wc_einvoice_settings', // Option group
+        'wc_einvoice_settings', // Option name
+        array(
+            'type' => 'array',
+            'sanitize_callback' => array($this, 'sanitize_settings')
+        )
+    );
+
+    register_setting(
+        'wc_einvoice_payment_settings', // Option group
+        'wc_einvoice_payment_settings', // Option name
+        array(
+            'type' => 'array',
+            'sanitize_callback' => array($this, 'sanitize_payment_settings')
+        )
+    );
+}
+
+public function sanitize_settings($input) {
+    $sanitized = array();
+    
+    if (isset($input['company_name'])) {
+        $sanitized['company_name'] = sanitize_text_field($input['company_name']);
+    }
+    
+    if (isset($input['tax_registration'])) {
+        $sanitized['tax_registration'] = sanitize_text_field($input['tax_registration']);
+    }
+    
+    if (isset($input['company_address'])) {
+        $sanitized['company_address'] = sanitize_textarea_field($input['company_address']);
+    }
+    
+    if (isset($input['invoice_prefix'])) {
+        $sanitized['invoice_prefix'] = sanitize_text_field($input['invoice_prefix']);
+    }
+    
+    if (isset($input['next_invoice_number'])) {
+        $sanitized['next_invoice_number'] = absint($input['next_invoice_number']);
+    }
+    
+    if (isset($input['default_tax_type'])) {
+        $sanitized['default_tax_type'] = sanitize_text_field($input['default_tax_type']);
+    }
+
+    return $sanitized;
+}
+
+public function sanitize_payment_settings($input) {
+    $sanitized = array();
+    
+    if (isset($input['payment_mode'])) {
+        $sanitized['payment_mode'] = sanitize_text_field($input['payment_mode']);
+    }
+    
+    if (isset($input['bank_details'])) {
+        $sanitized['bank_details'] = sanitize_textarea_field($input['bank_details']);
+    }
+    
+    if (isset($input['payment_terms'])) {
+        $sanitized['payment_terms'] = sanitize_textarea_field($input['payment_terms']);
+    }
+    
+    if (isset($input['enable_prepayment'])) {
+        $sanitized['enable_prepayment'] = (bool) $input['enable_prepayment'];
+    }
+    
+    if (isset($input['prepayment_percentage'])) {
+        $sanitized['prepayment_percentage'] = min(100, max(0, floatval($input['prepayment_percentage'])));
+    }
+
+    return $sanitized;
+}
+
